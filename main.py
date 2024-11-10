24a1841fb9c311cf2d4907045a3ecfba71c674da
thangquang09 <lythang09092004@gmail.com>
2024-11-10 15:29:54 +0700

    complete framing text data, add json data, prepare for readStream JSON

--- main.py
+++ main.py
@@ -1,32 +1,50 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, StringType, DoubleType, DateType
+from pyspark.sql.types import StructType, StructField, StringType, DateType
+from pyspark.sql.functions import udf
 import os
 from dotenv import load_dotenv
+
+from udf_utils import *
+
+def define_udfs():
+    return {
+        'extract_job_title_udf': udf(extract_job_title, StringType()),
+        'extract_salary_start_udf': udf(extract_salary_start, StringType()),
+        'extract_salary_end_udf': udf(extract_salary_end, StringType()),
+        'extract_experience_udf': udf(extract_experience, StringType()),
+        'extract_submission_deadline_udf': udf(extract_submission_deadline, DateType()),
+        'extract_job_description_udf': udf(extract_job_description, StringType()),
+        'extract_job_requirements_udf': udf(extract_job_requirements, StringType()),
+        'extract_benefits_udf': udf(extract_benefits, StringType()),
+        'extract_company_address_udf': udf(extract_company_address, StringType()),
+    }
 
 if __name__ == "__main__":
     load_dotenv()
     AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 
-    spark = SparkSession.builder.appName("AWS_Spark_Streaming")\
+    spark = (SparkSession.builder.appName("AWS_Spark_Streaming")
         .config('spark.jars.packages',
                 'org.apache.hadoop:hadoop-aws:3.3.1,'
-                'com.amazonaws:aws-java-sdk:1.11.469')\
-        .config('spark.hadoop.fs.s3a.impl', 'org.apache.hadoop.fs.s3a.S3AFileSystem')\
-        .config('spark.hadoop.fs.s3a.access.key', AWS_ACCESS_KEY_ID)\
-        .config('spark.hadoop.fs.s3a.secret.key', AWS_SECRET_ACCESS_KEY)\
-        .config('spark.hadoop.fs.s3a.aws.credentials.provider',
-                'org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider')\
+                'com.amazonaws:aws-java-sdk:1.11.469')
+        # .config('spark.hadoop.fs.s3a.impl', 'org.apache.hadoop.fs.s3a.S3AFileSystem')
+        # .config('spark.hadoop.fs.s3a.access.key', AWS_ACCESS_KEY_ID)
+        # .config('spark.hadoop.fs.s3a.secret.key', AWS_SECRET_ACCESS_KEY)
+        # .config('spark.hadoop.fs.s3a.aws.credentials.provider',
+        #         'org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider')
         .getOrCreate()
+    )
 
-    text_dir = 'file:///home/thangquang/Documents/CODE/aws-spark-streaming-project/data/csv'
+    text_dir = 'file:///home/thangquang/Documents/CODE/aws-spark-streaming-project/data/text'
     csv_dir = 'file:///home/thangquang/Documents/CODE/aws-spark-streaming-project/data/csv'
+    json_dir = 'file:///home/thangquang/Documents/CODE/aws-spark-streaming-project/data/json'
 
     data_schema = StructType([
         StructField("job_title", StringType(), True),
-        StructField("salary_start", DoubleType(), True),
-        StructField("salary_end", DoubleType(), True),
-        StructField("years_of_experience", DoubleType(), True),
+        StructField("salary_start", StringType(), True),
+        StructField("salary_end", StringType(), True),
+        StructField("years_of_experience", StringType(), True),
         StructField("submission_deadline", DateType(), True),
         StructField("job_description", StringType(), True),
         StructField("job_requirements", StringType(), True),
@@ -34,8 +52,40 @@ if __name__ == "__main__":
         StructField("company_address", StringType(), True),
     ])
 
-    
+    udfs = define_udfs()
 
+    text_df = spark.readStream\
+    .format("text")\
+    .option("wholetext", True)\
+    .load(text_dir)
+
+    text_df = text_df.withColumn("job_title", udfs["extract_job_title_udf"]('value'))
+    text_df = text_df.withColumn("salary_start", udfs["extract_salary_start_udf"]('value'))
+    text_df = text_df.withColumn("salary_end", udfs["extract_salary_end_udf"]('value'))
+    text_df = text_df.withColumn("years_of_experience", udfs["extract_experience_udf"]('value'))
+    text_df = text_df.withColumn("submission_deadline", udfs["extract_submission_deadline_udf"]('value'))
+    text_df = text_df.withColumn("job_description", udfs["extract_job_description_udf"]('value'))
+    text_df = text_df.withColumn("job_requirements", udfs["extract_job_requirements_udf"]('value'))
+    text_df = text_df.withColumn("benefits", udfs["extract_benefits_udf"]('value'))
+    text_df = text_df.withColumn("company_address", udfs["extract_company_address_udf"]('value'))
     
-    
-    
+    final_text_df = text_df.select("job_title",
+                                    "salary_start",
+                                    "salary_end",
+                                    "years_of_experience",
+                                    "submission_deadline",
+                                    "job_description",
+                                    "job_requirements",
+                                    "benefits",
+                                    "company_address",
+                                )
+
+    query = (
+        final_text_df.writeStream
+        .outputMode('append')
+        .format('console')
+        .option('truncate', False)
+        .start()
+    )
+
+    query.awaitTermination()
--- udf_utils.py
+++ udf_utils.py
@@ -1,26 +1,94 @@
+import re
+from datetime import datetime
+
 def extract_job_title(file_content):
-    pass
+    file_content = file_content.strip()
+    job_title = file_content.split("\n")[0]
+    return job_title
 
 def extract_salary_start(file_content):
-    pass
+    try:
+        pattern = r'Mức lương:\s*(\d+\s*triệu|\d+\s*-\s*\d+\s*triệu|Thỏa thuận)'
+        salary = re.findall(pattern, file_content, re.IGNORECASE)
+        
+        if " - " in salary[0]:
+            return salary[0].split(" - ")[0] + " triệu"
+        elif "thỏa thuận" in salary[0].lower():
+            return "Thỏa thuận"
+        else:
+            return salary[0]
+
+    except Exception as e:
+        raise ValueError(f"Error extracting salary start: {e}")
 
 def extract_salary_end(file_content):
-    pass
+    try:
+        pattern = r'Mức lương:\s*(\d+\s*triệu|\d+\s*-\s*\d+\s*triệu|Thỏa thuận)'
+        salary = re.findall(pattern, file_content, re.IGNORECASE)
+        
+        if " - " in salary[0]:
+            return salary[0].split(" - ")[1]
+        elif "thỏa thuận" in salary[0].lower():
+            return "Thỏa thuận"
+        else:
+            return salary[0]
+
+    except Exception as e:
+        raise ValueError(f"Error extracting salary end: {e}")
 
 def extract_experience(file_content):
-    pass
+    try:
+        pattern = r'Kinh nghiệm:\s*(\d+\s*năm)'
+        experience = re.findall(pattern, file_content, re.IGNORECASE)
+
+        return experience[0]
+    except Exception as e:
+        raise ValueError(f"Error extracting experience: {e}")
 
 def extract_submission_deadline(file_content):
-    pass
+    try:
+        file_content = file_content.strip()
+        deadline = file_content.split("\n")[4].split(": ")[1]
+        deadline = datetime.strptime(deadline, "%d/%m/%Y")
+        return deadline
+    except Exception as e:
+        raise ValueError(f"Error extracting submission deadline: {e}")
+
 
 def extract_job_description(file_content):
-    pass
+    try:
+        pattern = r'MÔ TẢ CÔNG VIỆC\n(.*?)(?=\nYÊU CẦU ỨNG VIÊN)'
+        description = re.search(pattern, file_content, re.DOTALL)
+        return description.group(1)
+    except Exception as e:
+        raise ValueError(f"Error extracting job description: {e}")
 
 def extract_job_requirements(file_content):
-    pass
+    try:
+        pattern = r'YÊU CẦU ỨNG VIÊN\n(.*?)(?=\nQUYỀN LỢI)'
+        requirements = re.search(pattern, file_content, re.DOTALL)
+        return requirements.group(1)
+    except Exception as e:
+        raise ValueError(f"Error extracting job requirements: {e}")
 
 def extract_benefits(file_content):
-    pass
+    try:
+        pattern = r'QUYỀN LỢI\n(.*?)(?=\nĐỊA ĐIỂM LÀM VIỆC)'
+        benefits = re.search(pattern, file_content, re.DOTALL)
+        return benefits.group(1)
+    except Exception as e:
+        raise ValueError(f"Error extracting benefits: {e}")
 
 def extract_company_address(file_content):
-    pass
+    try:
+        pattern = r'ĐỊA ĐIỂM LÀM VIỆC\n(.*)'
+        address = re.search(pattern, file_content, re.DOTALL)
+        return address.group(1)
+    except Exception as e:
+        raise ValueError(f"Error extracting address: {e}")
+
+if __name__ == "__main__":
+    with open("data/testdata/11238_NHAN_VIEN_KY_THUAT_DIEN.txt", 'r') as f:
+        content = f.read()
+        # print(content)
+        print(extract_company_address(content))
